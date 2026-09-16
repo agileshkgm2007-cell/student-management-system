fd697f26faf2eee40f353ce4bc0e3816da888677
agileshkgm2007-cell <agileshkgm2007@gmail.com>
2026-09-16 11:20:22 +0530

    Fix CORS security settings

--- backend/student_project/settings.py
+++ backend/student_project/settings.py
@@ -68,4 +68,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
